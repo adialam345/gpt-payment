2e3ba6cd0ee6eccf130d1fa5570974ab04811f7b
Your Name <your.email@example.com>
2026-05-12 23:36:41 +0700

    init

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  devToolbar: {
+    enabled: false
+  },
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'https://ezweystock.petrix.id',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    }
+  }
+});
